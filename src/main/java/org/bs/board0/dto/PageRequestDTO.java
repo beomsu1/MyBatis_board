8708a26682530f17a6e90acdd25d4c5734ebab20
Beomsu <beumsu1221@gmail.com>
2023-07-26 00:43:46 +0900

    board

--- src/main/java/org/bs/board0/dto/PageRequestDTO.java
+++ src/main/java/org/bs/board0/dto/PageRequestDTO.java
@@ -1,12 +1,16 @@
 package org.bs.board0.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @Builder
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageRequestDTO {
 
     @Builder.Default
