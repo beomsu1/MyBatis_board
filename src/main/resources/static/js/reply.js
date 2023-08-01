d237a9f4b309b9ab397bb8b4b755b7fe16e822b7
Beomsu <beumsu1221@gmail.com>
2023-08-01 15:49:28 +0900

    reply

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -3,10 +3,14 @@
 const url = `http://localhost:8080/api/replies/`
 
 // list
-
+// replyLast - false -> 댓글 마지막 페이지로 이동
+const replyGetList = async(replyLast = false , page= 1) => {
+    const res = await axios.get(`${url}${tno}/list/?page=${page}&replyLast=${replyLast}`)
+    return res.data
+}
 
 // regist
-const replyRegist = async(data) => {
+const replyPostRegist = async(data) => {
     const res = await axios.post(`${url}${tno}/regist`,data)
     return res.data
 }
