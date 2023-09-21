fc46ff9629a48ffecb5b9bc282ce70677e5aa7ec
Beomsu <beumsu1221@gmail.com>
2023-09-22 01:31:05 +0900

    구조 변경

--- src/main/java/org/bs/board0/dto/MemberModifyDTO.java
+++ src/main/java/org/bs/board0/dto/MemberModifyDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class MemberUpdateDTO {
+public class MemberModifyDTO {
     
     private String email;
     private String mpw;
